0081388575773b7c4de44bc77f201887094c567c
joao luis <joao.lc12@hotmail.com>
2024-08-20 10:30:19 -0300

    teste

---
+++ app/src/main/java/br/edu/ifsuldeminas/mch/controlefinanceiro/MainActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsuldeminas.mch.controlefinanceiro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+}
